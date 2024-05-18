b2a862f0026df405bee29fcae92ddc8a7f46f2d9
tyzrichard <tyzrichard28@gmail.com>
2024-05-18 23:53:43 +0800

    Finish Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) { //...args groups all other parameters into an array. useful.
+    return array.filter(x => !args.includes(x))
+    //filter removes everything that does not fit the criteria
+    //includes is kinda self-explanatory
 };
 
 // Do not edit below this line
