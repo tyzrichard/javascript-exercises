697f00812196d6fa79b1bdfb7be5ace529412cec
tyzrichard <tyzrichard28@gmail.com>
2024-05-20 16:21:48 +0800

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let start, finish, sum = 0;
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) {
+        return "ERROR";
+    }
+    if (num1 >= 0 && num2 >= 0) {
+        if (num1 > num2) {
+            start = num2;
+            finish = num1;
+        } else {
+            start = num1;
+            finish = num2;
+        }
+    } else {
+        return "ERROR"
+    }
 
+    for (i = start; i <= finish; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
