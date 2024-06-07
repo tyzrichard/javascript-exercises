b0ad99f70db67fd8b8f64b9894ea5565905c7b9d
tyzrichard <tyzrichard28@gmail.com>
2024-06-07 12:20:29 +0800

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    cleanedString = string.toLowerCase().split('').filter(char => /^[a-z0-9]*$/gi.test(char)).join('');
+    reversed = cleanedString.split('').reverse().join('');
+    return cleanedString === reversed;
 };
 
 // Do not edit below this line
