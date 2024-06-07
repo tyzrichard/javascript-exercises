c13d4b329d0c3e640351b0a46601d07eee03064a
tyzrichard <tyzrichard28@gmail.com>
2024-06-07 15:45:23 +0800

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldest = people[0]
+    people.forEach((person) => {
+        if (typeof person.yearOfDeath == 'undefined') {
+            person.yearOfDeath = parseInt(new Date().getFullYear())
+        }
+        if ((person.yearOfDeath - person.yearOfBirth) > oldest.yearOfDeath - oldest.yearOfBirth) {
+            oldest = person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
