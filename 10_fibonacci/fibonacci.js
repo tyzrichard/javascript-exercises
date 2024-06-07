f3314dc45684f114fe9bdba338b46335a5df88dc
tyzrichard <tyzrichard28@gmail.com>
2024-06-07 14:43:32 +0800

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num === 'string') {
+        num = parseInt(num);
+    }
+    if (num < 0) {return "OOPS"}
+    else if (num == 0) {
+        return 0;
+    }
+    else if (num <= 2) {
+        return 1;
+    }
+    let lastlastNumber = 1, lastNumber = 1, number;
+    for (i = 2; i < num; i++) {
+        number = lastlastNumber + lastNumber;
+        lastlastNumber = lastNumber;
+        lastNumber = number;
+    }
+    return number
 };
 
 // Do not edit below this line
